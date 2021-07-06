1a3bea6b7dced6e56534ea99b285987be16282eb
liwei <wei.li3@yintech.cn>
2021-07-06 13:43:42 +0800

    新增别名

--- customCommand.sh
+++ customCommand.sh
@@ -3,26 +3,26 @@
 #获取当前Activity
 alias adbtop="adb shell dumpsys window | grep mCurrentFocus"
 #卸载系统应用  adbust pkg
+alias adbist="adb install"
 alias adbust="adb shell pm unistall -k --user"
 #获取错误信息
 alias adbrunexp="adb logcat -v time | grep -i RuntimeException"
 alias adbexp="adb logcat -v time | grep -i exception"
-
-
+alias adbcdata="adb shell pm clear"
+# 启动activity  adbstart [applicationId/package.Activity]
+alias adbstart="adb shell am start -W"
 
 #git
 alias g="git"
 alias ga="git add"
 alias gaa="git add ."
 alias gb="git branch"
-alias gbD="git branch -D"
-alias gba="git branch -a"
 alias gbd="git branch -d"
+alias gbdf="git branch -D"
+alias gbdr="git push origin --delete"
 alias gck="git checkout"
-alias gst="git status"
 alias gct="git commit"
 alias gctm="git commit -m"
-alias gcam="git commit --amend -m"
 alias grb="git rebase"
 alias grba="git rebase --abort"
 alias grac="git rebase --continue"
@@ -31,8 +31,14 @@ alias gp="git push"
 alias gpf="git push -f"
 alias glg="git log"
 alias gtg="git tag"
-alias gdt="git -d tag"
+alias gtd="git -d tag"
+alias gtdr="git push origin --delete"
 alias gtp="git push origin"
 alias gsh="git stash"
+alias gshu="git stash -u"
 alias gshpop="git stash pop"
-alias gc="git clone"
+alias gcl="git clean -df"
+
+
+#gradle
+alias gwdpds="./gradlew :app:dependences"
